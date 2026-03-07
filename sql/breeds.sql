d7613f82b901adfc82cb41504b45b4fab460294d
Butterscotch Stallion <swift.plan5016@fastmail.com>
2026-03-07 09:26:16 -0500

    feat: add breeds SQL

---
+++ sql/breeds.sql
@@ -0,0 +1,40 @@
+INSERT INTO cat_breeds (name)
+VALUES ('Abyssinian');
+INSERT INTO cat_breeds (name)
+VALUES ('Bengal');
+INSERT INTO cat_breeds (name)
+VALUES ('Birman');
+INSERT INTO cat_breeds (name)
+VALUES ('British Shorthair');
+INSERT INTO cat_breeds (name)
+VALUES ('Burmese');
+INSERT INTO cat_breeds (name)
+VALUES ('Devon Rex');
+INSERT INTO cat_breeds (name)
+VALUES ('Egyptian Mau');
+INSERT INTO cat_breeds (name)
+VALUES ('Maine Coon');
+INSERT INTO cat_breeds (name)
+VALUES ('Manx');
+INSERT INTO cat_breeds (name)
+VALUES ('Norwegian Forest Cat');
+INSERT INTO cat_breeds (name)
+VALUES ('Ocicat');
+INSERT INTO cat_breeds (name)
+VALUES ('Persian');
+INSERT INTO cat_breeds (name)
+VALUES ('Ragdoll');
+INSERT INTO cat_breeds (name)
+VALUES ('Russian Blue');
+INSERT INTO cat_breeds (name)
+VALUES ('Scottish Fold');
+INSERT INTO cat_breeds (name)
+VALUES ('Siamese');
+INSERT INTO cat_breeds (name)
+VALUES ('Siberian');
+INSERT INTO cat_breeds (name)
+VALUES ('Sphynx');
+INSERT INTO cat_breeds (name)
+VALUES ('Tonkinese');
+INSERT INTO cat_breeds (name)
+VALUES ('Turkish Angora');
