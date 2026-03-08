6509f31e75840fb1a95791cd5c27f5c229267dff
Butterscotch Stallion <swift.plan5016@fastmail.com>
2026-03-08 00:06:54 -0500

    chore: remove duplicate mod declaration

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::{fmt, EnvFilter};
 
-mod hasher;
-
 pub fn setup_tracing() {
     tracing_subscriber::registry()
         .with(fmt::layer().pretty())
