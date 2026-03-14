b7a6cdd5778f7ed6f41783d0005260206c652ceb
Butterscotch Stallion <swift.plan5016@fastmail.com>
2026-03-14 02:29:33 -0400

    refactor: include credentials in API requests

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,8 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
+import { client } from "./api/client.gen";
 import { App } from "./App";
 import "./index.css";
+
+client.setConfig({
+  credentials: "include",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
